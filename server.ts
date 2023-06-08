adf90cdc0027eb5abb9c7c07d6063806a5d5acf3
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-09 09:36:26 +1000

    Add body parser in server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,28 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+
+const bodyParser = require("body-parser");
+
+let model = "";
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
+app.get("/", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
 app.get("/upload", (req, res) => {
   res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+app.post("/upload", (req, res) => {
+  const r = req.body;
+  console.log("body:", r);
+  res.status(200).json({ make: "Tesla", model: "Model 3" });
 });
