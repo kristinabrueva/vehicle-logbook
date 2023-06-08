7d9a5c9b92ce1669dc6e6a7a7e88f80e0e3ac054
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-09 09:34:04 +1000

    Create Button component

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps {
+  id?: any;
+  inline?: Boolean;
+  text: String;
+  type?: "button" | "submit" | "reset" | undefined;
+  handleClick?: (e: any) => void;
+}
+
+const Button = ({
+  id,
+  inline,
+  type,
+  text,
+  handleClick,
+}: ButtonProps): JSX.Element => (
+  <>
+    <button
+      type={type}
+      className={`border-[1px] rounded-sm text-sm px-2 bg-gray-100 border-black py-1 max-w-fit ${
+        inline ? "inline" : "block"
+      }`}
+      id={id}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  </>
+);
+
+export default Button;
