67bf1e35e579e1e7a58db603a531edd4e8881d5a
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-08 13:27:06 +1000

    Create data.ts

---
+++ src/data.ts
@@ -0,0 +1,24 @@
+export const MODELS = {
+  Ford: {
+    Ranger: ["Raptor", "Raptor X", "Wildtrak"],
+    Falcon: ["XR6", "XR6 Turbo", "XR8"],
+    "Falcon Ute": ["XR6", "XR6 Turbo"],
+  },
+  bmw: {
+    "130d": ["xDrive 26 d", "xDrive 30d"],
+    "240i": ["xDrive30d", "xDrive50d"],
+    "320e": ["xDrive75d", "xDrive80d", "xDrive85d"],
+  },
+  Tesla: {
+    "Model 3": ["Performance", "Long Range", "Dual Motor"],
+    "Model X": ["Standart", "Long Range Dual Motor"],
+    "Model S": ["Standart", "Long Range", "Triple Motor"],
+  },
+};
+
+export const POPULAR_CARS = [
+  { make: "Tesla", badge: "Model 3", model: "Performance" },
+  { make: "bmw", badge: "130d", model: "xDrive 30d" },
+  { make: "Tesla", badge: "Model S", model: "Long Range" },
+  { make: "Ford", badge: "Ranger", model: "Raptor" },
+];
