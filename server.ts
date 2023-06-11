c8e4e93359efec46b048da5225373cf13211e1eb
Kristina Brueva <kristinabruuu@gmail.com>
2023-06-11 13:59:46 +1000

    Update server

--- server.ts
+++ server.ts
@@ -1,66 +1,58 @@
-const express = require("express");
-const fs = require("fs");
+import express from "express";
+import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
+interface MulterRequest extends Request {
+  file: any;
+}
 
 const app = express();
 const port = process.env.PORT || 4000;
-
-const bodyParser = require("body-parser");
-
-let makeValue = "";
-let modelValue = "";
-let badgeValue = "";
-
-let fileContent = fs.readFile(
-  "/Users/kris/logbook.txt",
-  "utf8",
-  (err, data) => {
-    if (err) {
-      console.error(err);
-      return "Error";
-    }
-    return data;
-  }
-);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// create a GET route
-app.get("/", (req, res) => {
-  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
-});
-app.get("/cors", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.send({ msg: "This has CORS enabled 🎈" });
-});
-
-// This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/upload", (req, res) => {
-  console.log({
-    make: makeValue,
-    model: modelValue,
-    badge: badgeValue,
-    // file: fileContent,
-  });
-  res.send({
-    make: makeValue,
-    model: modelValue,
-    badge: badgeValue,
-    // file: fileContent,
+// to show dinamic pages using pug engine
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// to parse FormData
+const upload = multer({ dest: "uploads/" });
+
+let car: { make: string; model: string; badge: string } = {
+  make: "",
+  model: "",
+  badge: "",
+};
+let fileContent: string[] = [""];
+
+// app.use(express.urlencoded({ extended: true }));
+
+// create a GET route
+app.get("/", (req: Request, res: Response) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+});
+
+app.get("/upload", async (req: Request, res: Response) => {
+  res.render("upload", {
+    title: "Uploads",
+    car: car,
+    logbook: fileContent,
   });
 });
-app.post("/upload", (req, res) => {
-  console.log("req", req.body);
-  const { make, model, badge, file } = req.body;
-  console.log(file);
-  console.log(make);
-  makeValue = make;
-  modelValue = model;
-  badgeValue = badge;
-  // res.send(JSON.stringify({ data: "here" }));
+
+app.post("/upload", upload.single("file"), (req: Request, res: Response) => {
+  car = req.body;
+  try {
+    fileContent = fs
+      .readFileSync((req as MulterRequest).file.path, "utf8")
+      .toString()
+      .split("\n");
+  } catch (e) {
+    return "Could not read the file";
+  }
   res.status(200).json(req.body);
 });
